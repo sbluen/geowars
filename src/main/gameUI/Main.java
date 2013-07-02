859f07ebccc25e29a4192c7fed4034cf9d04a8af
CaptainMarvel <sbluen153@yahoo.com>
2013-07-01 18:50:17 -0700

    add file as result of reorganization

---
+++ src/main/gameUI/Main.java
@@ -0,0 +1,41 @@
+package main.gameUI;
+
+import main.gameData.Record;
+import main.gameUI.Game;
+
+/**
+ * 
+ * @author Steven Bluen
+ *The main class that bootstraps everything
+ */
+public class Main {
+
+	/**The main method.
+	 * Put in this class because it makes things simpler
+	 * @param args does nothing for now.
+	 */
+	public static void main(String[] args){
+		Game game = new Game();
+		
+		//Wait until ready to load a game
+		while(!Record.loadGame && !Record.newGame);
+						
+		/* Loads a new game for a new user and a returning user */
+		if(Record.newGame)
+			game.newGame();
+				
+		/* Reloads the game for a returning user */
+		else if(Record.loadResume){
+			game.submenu.updateLevelLabel();
+			game.submenu.updateScoreLabel();
+			game.initEntitiesResume();
+		}
+			
+		game.submenu.setLoadBtnEnabled(false); /* Load button is no longer needed */
+		Record.beforeLoading = false;
+		game.requestFocus();
+		while(true)
+			game.gameLoop();
+	} //end of main()
+
+}
